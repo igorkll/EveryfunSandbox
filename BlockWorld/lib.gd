f3ef110f7229420de92bc2642559587307326454
igorkll <frerther@gmail.com>
2025-07-17 02:08:31 +0300

    backup

--- BlockWorld/lib.gd
+++ BlockWorld/lib.gd
@@ -5,13 +5,17 @@ static func spawnBlock(world, position):
 	body.position = position
 	
 	var collision_shape = CollisionShape3D.new()
-	var box_shape = BoxShape3D.new()  # Создаем форму коллизии куба
+	var box_shape = BoxShape3D.new()
 	collision_shape.shape = box_shape
 	body.add_child(collision_shape)
 		
 	var mesh_instance = MeshInstance3D.new()
-	var cube_mesh = BoxMesh.new()  # Создаем меш куба
+	var cube_mesh = BoxMesh.new()
 	mesh_instance.mesh = cube_mesh
 	body.add_child(mesh_instance)
+	
+	var material = StandardMaterial3D.new()
+	material.albedo_texture = preload("res://skybox.jpg")
+	mesh_instance.material_override = material
 	
 	world.add_child(body)
--- BlockWorld/world.gd
+++ BlockWorld/world.gd
@@ -3,6 +3,6 @@ extends Node
 var lib = preload("res://lib.gd")
 
 func _ready():
-	for ix in range(-128, 128):
-		for iz in range(-128, 128):
+	for ix in range(-32, 32):
+		for iz in range(-32, 32):
 			lib.spawnBlock(get_tree().root.get_node("world"), (Vector3) (ix, 0, iz))
