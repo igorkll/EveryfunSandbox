30c78f2d38fb229cfd9ecc12d78cf7e9eef6c3c1
igorkll <frerther@gmail.com>
2025-07-16 23:13:02 +0300

    Update player.gd

--- BlockWorld/player.gd
+++ BlockWorld/player.gd
@@ -4,10 +4,12 @@ var move_acceleration = 75
 var fall_acceleration = 75
 var jump_acceleration = 10
 
-var max_fall_velocity = 250
-var max_move_velocity = 15
+var max_fall_velocity = 200
+var max_move_velocity = 5
 
-var velocity_down = 0.1
+var velocity_down = 0.00001
+
+var jump = false
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
@@ -20,19 +22,22 @@ func _physics_process(delta):
 		direction.z -= 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z += 1
+	if Input.is_action_just_pressed("move_jump"):
+		jump = true
+	if Input.is_action_just_released("move_jump"):
+		jump = false
 		
 	var camera_basis = $Camera.global_transform.basis
 	var camera_direction = -camera_basis.z.normalized()
 	var camera_right = camera_basis.x.normalized()
 	var move_direction = (camera_direction * direction.z + camera_right * direction.x).normalized()
 
-	if Input.is_action_pressed("move_jump"):
-		velocity.y += jump_acceleration * delta
-
 	velocity.x += move_direction.x * move_acceleration * delta
 	velocity.z += move_direction.z * move_acceleration * delta
 
-	if not is_on_floor():
+	if jump:
+		velocity.y += jump_acceleration * delta
+	elif not is_on_floor():
 		velocity.y -= fall_acceleration * delta
 		
 	var velocity2d = Vector2 (velocity.x, velocity.z);
