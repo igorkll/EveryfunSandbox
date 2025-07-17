5729bf2f5e38d2566def5d309009295687bd64c7
igorkll <frerther@gmail.com>
2025-07-17 15:13:51 +0300

    backup

--- BlockWorld/scripts/game.gd
+++ BlockWorld/scripts/game.gd
@@ -1,6 +1,6 @@
 extends Node
 
-static func spawnBlock(world, position):
+static func placeBlock(world, position):
 	var body = StaticBody3D.new()
 	body.position = position
 	
@@ -15,7 +15,27 @@ static func spawnBlock(world, position):
 	body.add_child(mesh_instance)
 	
 	var material = StandardMaterial3D.new()
-	material.albedo_texture = preload("res://textures/skybox.jpg")
+	material.albedo_texture = preload("res://textures/sky.hdr")
+	mesh_instance.material_override = material
+	
+	world.add_child(body)
+
+static func spawnBlock(world, position):
+	var body = RigidBody3D.new()
+	body.position = position
+	
+	var collision_shape = CollisionShape3D.new()
+	var box_shape = BoxShape3D.new()
+	collision_shape.shape = box_shape
+	body.add_child(collision_shape)
+		
+	var mesh_instance = MeshInstance3D.new()
+	var cube_mesh = BoxMesh.new()
+	mesh_instance.mesh = cube_mesh
+	body.add_child(mesh_instance)
+	
+	var material = StandardMaterial3D.new()
+	material.albedo_texture = preload("res://textures/sky.hdr")
 	mesh_instance.material_override = material
 	
 	world.add_child(body)
--- BlockWorld/scripts/world.gd
+++ BlockWorld/scripts/world.gd
@@ -5,4 +5,7 @@ var game = preload("res://scripts/game.gd")
 func _ready():
 	for ix in range(-32, 32):
 		for iz in range(-32, 32):
-			game.spawnBlock(get_tree().root.get_node("world"), (Vector3) (ix, 0, iz))
+			game.placeBlock(get_tree().root.get_node("world"), (Vector3) (ix, 0, iz))
+	
+	for iy in range(32):
+		game.spawnBlock(get_tree().root.get_node("world"), (Vector3) (0, 10 + iy, 0))
