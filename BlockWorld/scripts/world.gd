526f9b1eadc276fefb8b791cbc745c35e620ed9d
igorkll <frerther@gmail.com>
2025-07-18 13:52:14 +0300

    Update world.gd

--- BlockWorld/scripts/world.gd
+++ BlockWorld/scripts/world.gd
@@ -7,6 +7,11 @@ func _ready():
 		blockManager.spawn($World, (Vector3) (10, 10 + iy * 2, 10), true, preload("res://blocks/den/script.gd"))
 		blockManager.spawn($World, (Vector3) (10, 10 + iy * 2, -10), true, preload("res://blocks/rainbox/script.gd"))
 		
+		blockManager.spawn($World, (Vector3) (0, 10 + iy * 2, 12), false, preload("res://blocks/pig/script.gd"))
+		blockManager.spawn($World, (Vector3) (12, 10 + iy * 2, 0), false, preload("res://blocks/tnt/script.gd"))
+		blockManager.spawn($World, (Vector3) (12, 10 + iy * 2, 12), false, preload("res://blocks/den/script.gd"))
+		blockManager.spawn($World, (Vector3) (12, 10 + iy * 2, -12), false, preload("res://blocks/rainbox/script.gd"))
+		
 	for ix in range(-32, 32):
 		for iz in range(-32, 32):
 			blockManager.spawn($World, (Vector3) (ix, 0, iz), false, preload("res://blocks/grass/script.gd"))
