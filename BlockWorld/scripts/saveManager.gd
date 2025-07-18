ab05c5fe18c024278a82d1964582b1730de76373
igorkll <frerther@gmail.com>
2025-07-19 00:26:45 +0300

    backup

--- BlockWorld/scripts/saveManager.gd
+++ BlockWorld/scripts/saveManager.gd
@@ -10,24 +10,14 @@ func _ready():
 
 static var save_name
 static var save_dir
+
 static var save_world
 static var save_world_dynamic
 
 static func getSavePath(name):
 	return "user://saves/" + name
 	
-static func rawload():
-	var file = FileAccess.open(save_dir + "/dynamic", FileAccess.READ)
-	if file:
-		var dynamic = bytes_to_var(file.get_buffer(file.get_length()))
-		for rigidBodyData in dynamic:
-			blockManager.spawn(rigidBodyData.p, rigidBodyData.r, true, rigidBodyData.n)
-		file.close()
-		
-static func exists(name):
-	return FileAccess.file_exists(getSavePath(save_name))
-
-static func loadOrCreate(name):
+static func _recreateTree():
 	save_world = node_main.get_node("world")
 	if save_world:
 		save_world.queue_free()
@@ -40,12 +30,27 @@ static func loadOrCreate(name):
 	save_world_dynamic.name = "dynamic"
 	save_world.add_child(save_world_dynamic)
 	
+static func exists(name):
+	return FileAccess.file_exists(getSavePath(name))
+	
+static func open(name):
+	_recreateTree()
 	save_name = name
 	save_dir = getSavePath(save_name)
-	if DirAccess.dir_exists_absolute(save_dir):
-		rawload()
-	else:
-		DirAccess.make_dir_recursive_absolute(save_dir)
+	
+	var file = FileAccess.open(save_dir + "/dynamic", FileAccess.READ)
+	if file:
+		var dynamic = bytes_to_var(file.get_buffer(file.get_length()))
+		for rigidBodyData in dynamic:
+			blockManager.spawn(rigidBodyData.p, rigidBodyData.r, true, rigidBodyData.n)
+		file.close()
+
+static func create(name):
+	_recreateTree()
+	save_name = name
+	save_dir = getSavePath(save_name)
+	
+	DirAccess.make_dir_recursive_absolute(save_dir)
 
 static func save():
 	var file = FileAccess.open(save_dir + "/dynamic", FileAccess.WRITE)
--- BlockWorld/scripts/world.gd
+++ BlockWorld/scripts/world.gd
@@ -1,21 +1,24 @@
 extends Node
 
 func _ready():
-	saveManager.loadOrCreate("default")
-	
-	for iy in range(32):
-		blockManager.spawn((Vector3) (0, 10 + iy * 2, 10), null, true, "pig")
-		blockManager.spawn((Vector3) (10, 10 + iy * 2, 0), null, true, "tnt")
-		blockManager.spawn((Vector3) (10, 10 + iy * 2, 10), null, true, "den")
-		blockManager.spawn((Vector3) (10, 10 + iy * 2, -10), null, true, "rainbow")
+	if not saveManager.exists("default"):
+		saveManager.create("default")
 		
-		blockManager.spawn((Vector3) (0, 10 + iy * 2, 12), null, false, "pig")
-		blockManager.spawn((Vector3) (12, 10 + iy * 2, 0), null, false, "tnt")
-		blockManager.spawn((Vector3) (12, 10 + iy * 2, 12), null, false, "den")
-		blockManager.spawn((Vector3) (12, 10 + iy * 2, -12), null, false, "rainbow")
-		
-	for ix in range(-32, 32):
-		for iz in range(-32, 32):
-			blockManager.spawn((Vector3) (ix, 0, iz), null, false, "grass")
+		for iy in range(32):
+			blockManager.spawn((Vector3) (0, 10 + iy * 2, 10), null, true, "pig")
+			blockManager.spawn((Vector3) (10, 10 + iy * 2, 0), null, true, "tnt")
+			blockManager.spawn((Vector3) (10, 10 + iy * 2, 10), null, true, "den")
+			blockManager.spawn((Vector3) (10, 10 + iy * 2, -10), null, true, "rainbow")
 			
-	saveManager.save()
+			blockManager.spawn((Vector3) (0, 10 + iy * 2, 12), null, false, "pig")
+			blockManager.spawn((Vector3) (12, 10 + iy * 2, 0), null, false, "tnt")
+			blockManager.spawn((Vector3) (12, 10 + iy * 2, 12), null, false, "den")
+			blockManager.spawn((Vector3) (12, 10 + iy * 2, -12), null, false, "rainbow")
+			
+		for ix in range(-32, 32):
+			for iz in range(-32, 32):
+				blockManager.spawn((Vector3) (ix, 0, iz), null, false, "grass")
+				
+		saveManager.save()
+	else:
+		saveManager.open("default")
