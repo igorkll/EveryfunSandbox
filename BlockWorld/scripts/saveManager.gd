9a21fb90e8eb82844709ad03dd0c3b6a2316b25b
igorkll <frerther@gmail.com>
2025-07-18 15:07:32 +0300

    Update saveManager.gd

--- BlockWorld/scripts/saveManager.gd
+++ BlockWorld/scripts/saveManager.gd
@@ -1,8 +1,15 @@
 extends Node
 
+static func getSavePath(name):
+	return "user://saves/" + name
+
 static func save(name):
-	var file = FileAccess.open("user://Saves/" + name + "/dynamicBlocks.json", FileAccess.WRITE)
+	var saveDir = getSavePath(name)
+	DirAccess.make_dir_recursive_absolute(saveDir)
+	var file = FileAccess.open(saveDir + "/dynamicBlocks.json", FileAccess.WRITE)
 	if file:
-		var dynamicBlocks = {}
+		var dynamicBlocks = {
+			"AS": true
+		}
 		file.store_string(JSON.stringify(dynamicBlocks))
 		file.close()
