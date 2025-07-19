8046b143de659ed27a6c86b902b1fed1b02b1c82
igorkll <frerther@gmail.com>
2025-07-19 03:55:16 +0300

    Update script.gd

--- EveryfunSandbox/blocks/den/script.gd
+++ EveryfunSandbox/blocks/den/script.gd
@@ -2,3 +2,17 @@ extends block
 
 static var texture = preload("texture.png")
 static var mesh = preload("res://mesh/single_texture_block.obj")
+
+func jump():
+	__rigid_body.apply_impulse(Vector3(0, 25, 0))
+
+func __init():
+	if "jump" in __data && __data.jump && __rigid_body:
+		jump()
+
+func __interact():
+	if __rigid_body:
+		jump()
+	else:
+		__data.jump = true
+		blockManager.toDynamic(self)
