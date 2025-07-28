8e634c6469b2fd14fe6da5badedc2234e4c5e046
igorkll <frerther@gmail.com>
2025-07-28 18:06:30 +0300

    backup

--- EveryfunSandbox/scripts/Chunk.gd
+++ EveryfunSandbox/scripts/Chunk.gd
@@ -24,36 +24,34 @@ func updateMesh():
 	add_child(meshlist)
 
 	var currentIndex = {}
-	for iz in range(chunkManager.chunkSize):
-		for iy in range(chunkManager.chunkSize):
-			for ix in range(chunkManager.chunkSize):
-				var blockname = array[ix + (iy * chunkManager.chunkSize) + (iz * chunkManager.chunkSize * chunkManager.chunkSize)]
+	for i in range(chunkManager.chunkSize * chunkManager.chunkSize * chunkManager.chunkSize):
+		var blockname = array[i]
+		
+		if blockname:
+			var position = Vector3(i % chunkManager.chunkSize, floor(i / chunkManager.chunkSize) % chunkManager.chunkSize, floor(i / chunkManager.chunkSize / chunkManager.chunkSize))
+			var multiMeshInstance:MultiMeshInstance3D
+			if meshlist.has_node(blockname):
+				multiMeshInstance = meshlist.get_node(blockname)
 				
-				if blockname:
-					var position = Vector3(ix, iy, iz)
-					var multiMeshInstance:MultiMeshInstance3D
-					if meshlist.has_node(blockname):
-						multiMeshInstance = meshlist.get_node(blockname)
-						
-					if not multiMeshInstance:
-						var _mesh = blockManager.getMeshAndMaterial(blockManager.getBlockscript(blockname))
-						
-						var multiMesh = MultiMesh.new()
-						multiMesh.transform_format = MultiMesh.TRANSFORM_3D
-						multiMesh.mesh = _mesh[0]
-						multiMesh.instance_count = usesCount[blockname]
-							
-						multiMeshInstance = MultiMeshInstance3D.new()
-						multiMeshInstance.name = blockname
-						multiMeshInstance.material_override = _mesh[1]
-						multiMeshInstance.multimesh = multiMesh
-						meshlist.add_child(multiMeshInstance)
+			if not multiMeshInstance:
+				var _mesh = blockManager.getMeshAndMaterial(blockManager.getBlockscript(blockname))
 				
-					var transform = Transform3D()
-					transform.origin = position
-					if not currentIndex.has(blockname):
-						currentIndex[blockname] = 0
+				var multiMesh = MultiMesh.new()
+				multiMesh.transform_format = MultiMesh.TRANSFORM_3D
+				multiMesh.mesh = _mesh[0]
+				multiMesh.instance_count = usesCount[blockname]
 					
-					multiMeshInstance.multimesh.set_instance_transform(currentIndex[blockname], transform)
-					currentIndex[blockname] += 1
+				multiMeshInstance = MultiMeshInstance3D.new()
+				multiMeshInstance.name = blockname
+				multiMeshInstance.material_override = _mesh[1]
+				multiMeshInstance.multimesh = multiMesh
+				meshlist.add_child(multiMeshInstance)
+		
+			var transform = Transform3D()
+			transform.origin = position
+			if not currentIndex.has(blockname):
+				currentIndex[blockname] = 0
+			
+			multiMeshInstance.multimesh.set_instance_transform(currentIndex[blockname], transform)
+			currentIndex[blockname] += 1
 	
