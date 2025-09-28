59debf4e419ef13d8dc8b2a61d3f6dd2d23e1096
igorkll <frerther@gmail.com>
2025-09-29 01:06:56 +0300

    Update terrain.gd

--- EveryfunSandbox/terrain.gd
+++ EveryfunSandbox/terrain.gd
@@ -1,13 +1,7 @@
 extends VoxelTerrain
 
 func _ready():
-	var noise = FastNoiseLite.new()
-	noise.seed = randi()
-	noise.frequency = 0.02
-	noise.fractal_octaves = 4
-	
-	var gen = VoxelGeneratorNoise.new()
-	gen.noise = noise
+	var gen = VoxelGeneratorWaves.new()
 	gen.height_range = 50
 	
 	var mesher = VoxelMesherBlocky.new()
@@ -27,10 +21,10 @@ func setup_block_library(library: VoxelBlockyLibrary):
 	
 	# Земля
 	var dirt = VoxelBlockyModel.new()
-	stone.color = Color("5d4037")
+	dirt.color = Color("5d4037")
 	library.add_model(dirt)
 	
 	# Трава
 	var grass = VoxelBlockyModel.new()
-	stone.color = Color("4caf50")
+	grass.color = Color("4caf50")
 	library.add_model(grass)
