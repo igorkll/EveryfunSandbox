9d0e8cc605e70e04f21f77d5d74bf23cd3760608
igorkll <frerther@gmail.com>
2025-09-29 01:01:38 +0300

    Update terrain.gd

--- EveryfunSandbox/terrain.gd
+++ EveryfunSandbox/terrain.gd
@@ -1,13 +1,36 @@
 extends VoxelTerrain
 
 func _ready():
-	var noise := FastNoiseLite.new()
+	var noise = FastNoiseLite.new()
 	noise.seed = randi()
 	noise.frequency = 0.02
 	noise.fractal_octaves = 4
 	
-	var gen := VoxelGeneratorNoise.new()
+	var gen = VoxelGeneratorNoise.new()
 	gen.noise = noise
+	gen.height_range = 50
 	
-	self.mesher = VoxelMesherTransvoxel.new()
+	var mesher = VoxelMesherBlocky.new()
+	
+	var library = VoxelBlockyLibrary.new()
+	setup_block_library(library)
+	mesher.library = library
+	
+	self.mesher = mesher
 	self.generator = gen
+
+func setup_block_library(library: VoxelBlockyLibrary):
+	# Камень
+	var stone = VoxelBlockyModel.new()
+	stone.color = Color("8b8b8b")
+	library.add_model(stone)
+	
+	# Земля
+	var dirt = VoxelBlockyModel.new()
+	stone.color = Color("5d4037")
+	library.add_model(dirt)
+	
+	# Трава
+	var grass = VoxelBlockyModel.new()
+	stone.color = Color("4caf50")
+	library.add_model(grass)
