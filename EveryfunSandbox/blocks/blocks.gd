358bd8525e566b373d39aa3e028eaa891550aa7a
igorkll <frerther@gmail.com>
2025-09-30 00:26:49 +0300

    Update blocks.gd

--- EveryfunSandbox/blocks/blocks.gd
+++ EveryfunSandbox/blocks/blocks.gd
@@ -1,33 +1,41 @@
 extends Node
 
 var shader
-var material
 var library
 
 var blocklist = [
 	{
-		texture = "res://blocks/stone/texture.png"
+		texture = preload("res://blocks/stone/texture.png"),
+		texture_mode = 1
 	}
 ]
 
+var textureModes = [
+	[
+		Vector2i(3, 3),
+		
+		Vector2i(0, 1),
+		Vector2i(2, 1),
+		Vector2i(0, 0),
+		Vector2i(1, 1),
+		Vector2i(1, 2),
+		Vector2i(1, 0)
+	],
+	[
+		Vector2i(1, 1),
+		
+		Vector2i(0, 0),
+		Vector2i(0, 0),
+		Vector2i(0, 0),
+		Vector2i(0, 0),
+		Vector2i(0, 0),
+		Vector2i(0, 0)
+	]
+]
+
 func _ready():
-	shader = load("res://blocks/blocks.gdshader")
-	material = _get_material()
+	shader = preload("res://blocks/blocks.gdshader")
 	library = _get_library()
-
-
-
-
-func _generate_atlas():
-	for block in blocklist:
-		block.atlas_size = Vector2i(2, 2)
-	
-func _get_material():
-	var material = ShaderMaterial.new()
-	material.shader = shader
-	material.set_shader_parameter("diff_texture", _generate_atlas())
-	
-	return material
 
 func _get_library():
 	var library = VoxelBlockyLibrary.new()
@@ -36,15 +44,21 @@ func _get_library():
 	library.add_model(air)
 	
 	for block in blocklist:
+		var material = ShaderMaterial.new()
+		material.shader = shader
+		material.set_shader_parameter("diff_texture", block.texture)
+		
+		var textureMode = textureModes[block.texture_mode]
+		
 		var blockModel = VoxelBlockyModelCube.new()
-		blockModel.atlas_size_in_tiles = block.atlas_size
+		blockModel.atlas_size_in_tiles = textureMode[0]
 		blockModel.set_material_override(0, material)
-		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_X, block.atlas_neg_x)
-		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_X, block.atlas_pos_x)
-		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_Y, block.atlas_neg_y)
-		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_Y, block.atlas_pos_y)
-		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_Z, block.atlas_neg_z)
-		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_Z, block.atlas_pos_z)
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_X, textureMode[1])
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_X, textureMode[2])
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_Y, textureMode[3])
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_Y, textureMode[4])
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_Z, textureMode[5])
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_Z, textureMode[6])
 		library.add_model(blockModel)
 	
 	return library
