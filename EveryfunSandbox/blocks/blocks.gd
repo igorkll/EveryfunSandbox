01382fe8e252bf2bda4c926bf5ed89a0eb2db348
igorkll <frerther@gmail.com>
2025-09-29 23:03:12 +0300

    rewriting...

--- EveryfunSandbox/blocks/blocks.gd
+++ EveryfunSandbox/blocks/blocks.gd
@@ -1,18 +1,33 @@
 extends Node
 
-var atlas_size = Vector2i(2, 2)
+var shader
 var material
 var library
 
-var blockslist = [
-	
+var blocklist = [
+	{
+		texture = "res://blocks/stone/texture.png"
+	}
 ]
 
 func _ready():
-	material = ShaderMaterial.new()
-	material.shader = load("res://blocks/blocks.gdshader")
-	
+	shader = load("res://blocks/blocks.gdshader")
+	material = _get_material()
 	library = _get_library()
+
+
+
+
+func _generate_atlas():
+	for block in blocklist:
+		block.atlas_size = Vector2i(2, 2)
+	
+func _get_material():
+	var material = ShaderMaterial.new()
+	material.shader = shader
+	material.set_shader_parameter("diff_texture", _generate_atlas())
+	
+	return material
 
 func _get_library():
 	var library = VoxelBlockyLibrary.new()
@@ -20,15 +35,16 @@ func _get_library():
 	var air = VoxelBlockyModelEmpty.new()
 	library.add_model(air)
 	
-	var block = VoxelBlockyModelCube.new()
-	block.atlas_size_in_tiles = atlas_size
-	block.set_material_override(0, blocks.material)
-	library.add_model(block)
-	
-	var block2 = VoxelBlockyModelCube.new()
-	block2.atlas_size_in_tiles = atlas_size
-	block2.set_material_override(0, blocks.material)
-	block2.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_Y, Vector2i(1, 1))
-	library.add_model(block2)
+	for block in blocklist:
+		var blockModel = VoxelBlockyModelCube.new()
+		blockModel.atlas_size_in_tiles = block.atlas_size
+		blockModel.set_material_override(0, material)
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_X, block.atlas_neg_x)
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_X, block.atlas_pos_x)
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_Y, block.atlas_neg_y)
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_Y, block.atlas_pos_y)
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_NEGATIVE_Z, block.atlas_neg_z)
+		blockModel.set_tile(VoxelBlockyModel.Side.SIDE_POSITIVE_Z, block.atlas_pos_z)
+		library.add_model(blockModel)
 	
 	return library
