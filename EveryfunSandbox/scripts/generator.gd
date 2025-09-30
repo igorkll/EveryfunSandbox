9ef5f07e3e7bedec79a48abe37189d2a52724430
igorkll <frerther@gmail.com>
2025-09-30 03:14:26 +0300

    Update generator.gd

--- EveryfunSandbox/scripts/generator.gd
+++ EveryfunSandbox/scripts/generator.gd
@@ -1,13 +1,12 @@
 extends VoxelGeneratorScript
 
 func _generate_block(buffer: VoxelBuffer, position: Vector3i, lod: int):
-	var div = lod + 1
-	
 	var size = buffer.get_size()
-	for ix in range(0, size.x / div):
-		for iy in range(0, size.y / div):
-			for iz in range(0, size.z / div):
-				if (iy + div) + position.y == 10:
+	for ix in range(0, size.x):
+		for iy in range(0, size.y):
+			for iz in range(0, size.z):
+				var pos = position + Vector3i(ix, iy, iz)
+				if pos.y == 10:
 					buffer.set_voxel(1, ix, iy, iz, VoxelBuffer.CHANNEL_TYPE)
 				else:
 					buffer.set_voxel(0, ix, iy, iz, VoxelBuffer.CHANNEL_TYPE)
