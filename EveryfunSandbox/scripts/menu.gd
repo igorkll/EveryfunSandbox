3cf1d18d7f39468f933275a5cab7e63f813dfe73
igorkll <frerther@gmail.com>
2025-10-02 02:55:07 +0300

    backup

---
+++ EveryfunSandbox/scripts/menu.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var menuUI
+var gameUI
+
+func openUI(gameUImode):
+	menuUI.visible = not gameUImode
+	gameUI.visible = gameUImode
+
+func _ready():
+	menuUI = get_node("/root/main/gui/container/menuUI")
+	gameUI = get_node("/root/main/gui/container/gameUI")
+	
+	openUI(false)
