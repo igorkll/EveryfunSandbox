f5d12d44b12bd29134358b684220b03e7b60ade4
igorkll <frerther@gmail.com>
2025-10-01 04:43:56 +0300

    saving test

--- EveryfunSandbox/scripts/terrain.gd
+++ EveryfunSandbox/scripts/terrain.gd
@@ -6,7 +6,11 @@ func _ready():
 	var mesher = VoxelMesherBlocky.new()
 	mesher.library = game.blockLibrary
 	
+	var stream = VoxelStreamSQLite.new()
+	stream.database_path = "user://terrain.db"
+	
 	self.mesher = mesher
 	self.generator = world_generator.new()
-	self.view_distance = 2048
-	self.lod_distance = 256
+	self.view_distance = 128
+	self.lod_distance = 64
+	self.stream = stream
