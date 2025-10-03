b3f42fecd25ab1cb5126bdb1c9db898710a14a6b
igorkll <frerther@gmail.com>
2025-10-03 20:00:02 +0300

    Update consts.gd

--- EveryfunSandbox/scripts/consts.gd
+++ EveryfunSandbox/scripts/consts.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var minimum_loading_radius_for_play = Vector3i(25, 10, 25)
-var player_spawn_vertical_offset = 0.05
+var player_spawn_vertical_offset = 0.1
 var settings_path = "user://settings.json"
 var base_mouse_sensitivity = 0.2
 var base_joystick_camera_sensitivity = 200
