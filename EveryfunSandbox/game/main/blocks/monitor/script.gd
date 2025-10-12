89c744944edb5524d40a610949f22be169e24a2a
igorkll <frerther@gmail.com>
2025-10-13 02:59:14 +0300

    Update script.gd

--- EveryfunSandbox/game/main/blocks/monitor/script.gd
+++ EveryfunSandbox/game/main/blocks/monitor/script.gd
@@ -6,10 +6,10 @@ func _ready():
 	var plane := MeshInstance3D.new()
 	plane.mesh = PlaneMesh.new()
 	plane.scale = Vector3(0.5, 0.5, 0.5) * 0.8
-	plane.rotation_degrees = Vector3(0, 0, -90)
+	plane.rotation_degrees = Vector3(90, 0, -90)
 	plane.position = Vector3(0.51, 0, 0)
 	add_child(plane)
 
 	var material := StandardMaterial3D.new()
-	# material.albedo_texture = viewport.get_texture()
+	material.albedo_texture = get_tree().root.get_texture()
 	plane.material_override = material
