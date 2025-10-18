7758e844e9330b75aba615468b74edbb99ba629b
igorkll <frerther@gmail.com>
2025-10-18 22:05:47 +0300

    backup

--- EveryfunSandbox/game/main/scripts/grammophone.gd
+++ EveryfunSandbox/game/main/scripts/grammophone.gd
@@ -6,15 +6,36 @@ var defaultStorageData = {
 	rpm = 78.26
 }
 
-var audioPlayer
+var eff_bg = preload("res://game/main/sounds/grammophone/background.mp3")
+var eff_border = preload("res://game/main/sounds/grammophone/disk_border.mp3")
+
+var audioPlayer: AudioStreamPlayer3D
+var audioPlayerEffect: AudioStreamPlayer3D
 var rotationCount = 0
 
 func __updateSound():
 	audioPlayer.pitch_scale = (storageData.rpm + (sin(rotationCount * PI * 2) * sinDestortion)) / defaultStorageData.rpm
 
-func __loadStream(path):
+func __play(path):
+	audioPlayerEffect.stream = eff_bg
+	audioPlayerEffect.play()
+	
 	audioPlayer.stream = game.loadResource(path)
 	audioPlayer.play()
+	
+func __stop():
+	audioPlayer.stop()
+	audioPlayerEffect.stop()
+	
+func __initAudioPlayer(audioPlayer):
+	game.initAudioStream(audioPlayer)
+	audioPlayer.emission_angle_enabled = true
+	audioPlayer.emission_angle_degrees = 45
+	audioPlayer.emission_angle_filter_attenuation_db = -30
+
+func __disk_end():
+	audioPlayerEffect.stream = eff_border
+
 
 func _ready():
 	var node = Node3D.new()
@@ -22,10 +43,13 @@ func _ready():
 	
 	audioPlayer = AudioStreamPlayer3D.new()
 	audioPlayer.bus = "Grammophone"
-	game.initAudioStream(audioPlayer)
-	audioPlayer.emission_angle_enabled = true
-	audioPlayer.emission_angle_degrees = 45
-	audioPlayer.emission_angle_filter_attenuation_db = -30
+	__initAudioPlayer(audioPlayer)
+	audioPlayer.connect("finished", __disk_end)
+	
+	audioPlayerEffect = AudioStreamPlayer3D.new()
+	audioPlayerEffect.bus = "Effects"
+	__initAudioPlayer(audioPlayerEffect)
+	
 	__updateSound()
 
 	node.add_child(audioPlayer)
@@ -39,4 +63,4 @@ func _requestDefaultStorageData():
 	return defaultStorageData
 
 func _use():
-	game.requestFile([consts.extfilter_audio], __loadStream)
+	game.requestFile([consts.extfilter_audio], __play)
