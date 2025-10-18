8953e3366abc82e1b481e0eaefbf06b9b0ee228c
igorkll <frerther@gmail.com>
2025-10-18 18:02:03 +0300

    Update advanced.gd

--- EveryfunSandbox/scripts/advanced.gd
+++ EveryfunSandbox/scripts/advanced.gd
@@ -5,15 +5,18 @@ func explode(position, explosiveLevel):
 		return
 	
 	var raycastDistance = explosiveLevel * 2
-	var iterations = explosiveLevel * 1
 	var shrapnel = explosiveLevel * 4
+	
+	var explosionState = {
+		"iterations": explosiveLevel * 1
+	}
 	
 	timers.setInterval(func():
 		for i in range(shrapnel):
 			var result = terrainUtils.blockRaycast(position, funcs.getRandomDirection(), raycastDistance)
 			if result:
 				terrainUtils.destroyBlock(result[0], result[1].position)
-		iterations -= 1
-		if iterations < 1:
+		explosionState.iterations -= 1
+		if explosionState.iterations < 1:
 			return true
 	, 0.1)
