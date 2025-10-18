7b4463810499cc33dba1c9b3a45e14c4cdc70958
igorkll <frerther@gmail.com>
2025-10-18 19:59:59 +0300

    backup

--- EveryfunSandbox/game/main/scripts/grammophone.gd
+++ EveryfunSandbox/game/main/scripts/grammophone.gd
@@ -1,10 +1,15 @@
 extends baseblock
 
+var audioPlayer
+
 func _ready():
-	var audioPlayer = AudioStreamPlayer3D.new()
+	audioPlayer = AudioStreamPlayer3D.new()
 	audioPlayer.bus = "Grammophone"
 	audioPlayer.stream = preload("res://game/main/music/8.mp3")
 	game.initAudioStream(audioPlayer)
 
 	add_child(audioPlayer)
 	audioPlayer.play()
+
+func _process(delta):
+	audioPlayer.volume_db = -15
