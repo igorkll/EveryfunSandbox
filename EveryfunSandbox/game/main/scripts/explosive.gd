114bafdeee9a6ca381586bb9a9189282a37b6462
igorkll <frerther@gmail.com>
2025-10-20 02:38:02 +0300

    fix
    The volume of the explosion sound has been reduced

--- EveryfunSandbox/game/main/scripts/explosive.gd
+++ EveryfunSandbox/game/main/scripts/explosive.gd
@@ -1,15 +1,28 @@
 extends baseblock
 
+var timer
+var hasExploded = false
+
 func _ready():
 	pass
 	
 func __explode():
+	if hasExploded:
+		return
+	hasExploded = true
+	
+	timers.clearTimeout(timer)
+	
 	advanced.explode(global_position, scriptData.explosiveLevel)
 	game.playSound(game.soundList["explosion"], position)
 	destroy()
 
 func _use():
-	timers.setTimeout(__explode, 4)
+	if timer != null:
+		__explode()
+		return
+
+	timer = timers.setTimeout(__explode, 4)
 	
 func _explode():
 	__explode()
