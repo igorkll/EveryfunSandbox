bf9f33e725df2c85fc8e06f5a3c84023a84f2456
igorkll <frerther@gmail.com>
2025-10-23 11:32:28 +0300

    Update terrainInteractions.gd

--- EveryfunSandbox/scripts/terrainInteractions.gd
+++ EveryfunSandbox/scripts/terrainInteractions.gd
@@ -2,7 +2,6 @@ extends Node
 
 func placeBlock(terrain, position: Vector3i, blockId: int, rotation=0, variant=0, color=0, storageData=null):
 	var item = game.blockList[game.getVariantBlockId(blockId, rotation, variant, color)]
-	
 	if item.has("sound_place"):
 		game.playSound(game.soundList[item.sound_place], terrainUtils.getGlobalPositionFromVoxelPosition(terrain, position))
 	
