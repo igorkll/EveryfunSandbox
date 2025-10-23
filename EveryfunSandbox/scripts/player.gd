f99c1c0d854bbfbff8fe6679f729cbec6299fbd1
igorkll <frerther@gmail.com>
2025-10-23 11:27:31 +0300

    Update player.gd

--- EveryfunSandbox/scripts/player.gd
+++ EveryfunSandbox/scripts/player.gd
@@ -173,12 +173,12 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("attack"):
 			var result = terrainUtils.blockRaycast($camera.get_global_transform().origin, -$camera.get_transform().basis.z, max_interact_distance)
 			if result:
-				terrainUtils.destroyBlock(game.terrain, result[1].position)
+				terrainInteractions.destroyBlock(game.terrain, result[1].position)
 				
 		if Input.is_action_just_pressed("place"):
 			var result = terrainUtils.blockRaycast($camera.get_global_transform().origin, -$camera.get_transform().basis.z, max_interact_distance)
 			if result and terrainUtils.isCellFree(game.terrain, result[1].previous_position):
-				terrainUtils.placeBlock(game.terrain, result[1].previous_position, game.blockIDs["testVariants"], game.getBlockDefaultRotation($camera.global_transform.basis.z))
+				terrainInteractions.placeBlock(game.terrain, result[1].previous_position, game.blockIDs["grammophone"], game.getBlockDefaultRotation($camera.global_transform.basis.z))
 				
 		if Input.is_action_just_pressed("use"):
 			var result = terrainUtils.blockRaycast($camera.get_global_transform().origin, -$camera.get_transform().basis.z, max_interact_distance)
