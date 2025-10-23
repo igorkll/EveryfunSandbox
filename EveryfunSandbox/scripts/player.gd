af5595d1379523b7606fdfcfc94a2246ee97344c
igorkll <frerther@gmail.com>
2025-10-23 11:47:44 +0300

    backup

--- EveryfunSandbox/scripts/player.gd
+++ EveryfunSandbox/scripts/player.gd
@@ -178,7 +178,7 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("place"):
 			var result = terrainUtils.blockRaycast($camera.get_global_transform().origin, -$camera.get_transform().basis.z, max_interact_distance)
 			if result and terrainUtils.isCellFree(game.terrain, result[1].previous_position):
-				terrainInteractions.placeBlock(game.terrain, result[1].previous_position, game.blockIDs["grammophone"], game.getBlockDefaultRotation($camera.global_transform.basis.z))
+				terrainInteractions.placeBlock(game.terrain, result[1].previous_position, game.blockIDs["glow"], game.getBlockDefaultRotation($camera.global_transform.basis.z))
 				
 		if Input.is_action_just_pressed("use"):
 			var result = terrainUtils.blockRaycast($camera.get_global_transform().origin, -$camera.get_transform().basis.z, max_interact_distance)
