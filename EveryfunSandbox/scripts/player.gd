8657f0e6d72d8d02497f4299f50f3e9147ff1015
igorkll <frerther@gmail.com>
2025-10-29 12:25:26 +0300

    Update player.gd

--- EveryfunSandbox/scripts/player.gd
+++ EveryfunSandbox/scripts/player.gd
@@ -177,7 +177,7 @@ func _physics_process(delta):
 				terrainInteractions.destroyBlock(result[0], result[1].position)
 				
 				var body = saves.createBody(terrainUtils.getGlobalPositionFromVoxelPosition(result[0], result[1].position))
-				terrainUtils.placeBlock(body, Vector3i(0, 0, 0), blockUtils.list_name2id["testTempScript"])
+				terrainUtils.placeBlock(body, result[1].position, blockUtils.list_name2id["testTempScript"])
 				
 		if Input.is_action_just_pressed("place"):
 			var result = terrainUtils.blockRaycast($camera.get_global_transform().origin, -$camera.get_transform().basis.z, max_interact_distance)
