5a6734bcfffbea71e4c7084c9266a0100920f28a
igorkll <frerther@gmail.com>
2025-11-09 15:16:30 +0300

    backup

--- EveryfunSandbox/scripts/classes/player.gd
+++ EveryfunSandbox/scripts/classes/player.gd
@@ -12,6 +12,32 @@ func _ready():
 	camera.add_child(chunkloader.new())
 
 func _physics_process(delta):
-	
+	if not control_lock:
+		var result = raycast()
+		
+		if Input.is_action_just_pressed("attack"):
+			if result:
+				terrainInteractions.destroyBlock(result[0], result[1].position)
+				
+				var body = bodyUtils.createBody(terrainUtils.getGlobalPositionFromVoxelPosition(result[0], result[1].position) + Vector3(0, 15, 0))
+				terrainUtils.placeBlock(body, Vector3i(0, 0, 0), blockUtils.list_name2id["testTempScript"])
+				
+		if Input.is_action_just_pressed("place"):
+			if result and terrainUtils.isCellFree(result[0], result[1].previous_position):
+				terrainInteractions.placeBlock(result[0], result[1].previous_position, blockUtils.list_name2id["explosive"], blockUtils.getTargetRotation($camera.global_transform.basis.z))
+			
+		if result && terrainUtils.canUseBlock(result[0], result[1].position):
+			game.setCrosspiece("use")
+			if Input.is_action_just_pressed("use"):
+				terrainUtils.useBlock(result[0], result[1].position)
+		else:
+			game.setCrosspiece("normal")
+	else:
+		game.setCrosspiece("normal")
 	
 	super._physics_process(delta)
+
+func raycast():
+	var raycastPosition = camera.get_global_transform().origin
+	var raycastDirection = -camera.get_transform().basis.z
+	return terrainUtils.blockRaycast(raycastPosition, raycastDirection, consts.max_interact_distance)
--- EveryfunSandbox/scripts/consts.gd
+++ EveryfunSandbox/scripts/consts.gd
@@ -7,6 +7,7 @@ var chunk_size = 16
 
 var minimum_loading_radius_for_play = Vector3i(8, 8, 8)
 var player_spawn_vertical_offset = 0.1
+var max_interact_distance = 10
 
 var base_mouse_sensitivity = 0.2
 var base_joystick_camera_sensitivity = 200
