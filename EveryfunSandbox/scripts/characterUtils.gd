3840a6c655063cbc2c98f2a366c225079cca513d
igorkll <frerther@gmail.com>
2025-11-09 13:53:22 +0300

    backup

--- EveryfunSandbox/scripts/characterUtils.gd
+++ EveryfunSandbox/scripts/characterUtils.gd
@@ -1,7 +1,9 @@
 extends Node
 
+var characters = [preload("res://scripts/classes/player.gd")]
+
 func spawn(character):
 	game.characters.add_child(character)
 
 func loadCharacters():
-	spawn()
+	spawn(characters[0].new())
--- EveryfunSandbox/scripts/classes/basecharacter.gd
+++ EveryfunSandbox/scripts/classes/basecharacter.gd
@@ -5,12 +5,67 @@ var character_height
 # -------------------------------------------------
 
 var _inited = false
+var _collision: CollisionShape3D
+var _disable_collision = false
+var _direction = Vector3.ZERO
 
 func _physics_process(delta):
 	if not _inited || not saves.isWorldFullLoaded():
 		return
-		
 	
+	_collision.disabled = _disable_collision
+	
+	var player_basis = global_transform.basis
+	var player_direction = -player_basis.z
+	var player_right = player_basis.x.normalized()
+	
+	player_direction.y = 0
+	player_direction = player_direction.normalized()
+	
+	var move_direction = (player_direction * _direction.z + player_right * _direction.x)
+	move_direction.y = 0
+	
+	if move_direction.length() > 1:
+		move_direction = move_direction.normalized()
+
+	velocity.x += move_direction.x * _move_acceleration * delta
+	velocity.z += move_direction.z * _move_acceleration * delta
+
+	var on_floor = is_on_floor()
+	if not on_floor:
+		if not flyState:
+			velocity += get_gravity() * delta * fall_speed_mul
+	elif not _on_floor:
+		var voxel = getDownVoxelObj()
+		if voxel and voxel.has("sound_jump") and game.soundList.has(voxel.sound_jump):
+			blockSound(game.soundList[voxel.sound_jump])
+			
+	_on_floor = on_floor
+
+	if current_jump:
+		if flyState:
+			velocity.y += _move_acceleration * delta
+		else:
+			velocity.y += jump_acceleration
+		
+	if velocity.y > 0:
+		var voxel = getUpVoxelObj()
+		if voxel and voxel.has("sound_headbutt") and game.soundList.has(voxel.sound_headbutt) and headbuttSound:
+			blockSound(game.soundList[voxel.sound_headbutt])
+			headbuttSound = false
+	elif velocity.y < 0:
+		headbuttSound = true
+	
+	var speed_mul = pow(velocity_drop, delta);
+	velocity.x *= speed_mul;
+	if flyState:
+		velocity.y *= speed_mul;
+	velocity.z *= speed_mul;
+		
+	if not terrainUtils.isMinimalAreaLoaded(game.terrain, terrainUtils.getVoxelPositionFromGlobalPosition(game.terrain, position)):
+		velocity = Vector3(0, 0, 0)
+	
+	move_and_slide()
 
 # -------------------------------------------------
 
@@ -37,7 +92,9 @@ func _getVoxel(side):
 
 # ------------------------------------------------- api
 
-func init(collision, mesh):
+func init(collision: CollisionShape3D, mesh: Mesh):
+	_collision = collision
+	
 	add_child(collision)
 	
 	var meshIntance = MeshInstance3D.new()
@@ -49,6 +106,9 @@ func init(collision, mesh):
 
 func apply_impulse(direction: Vector3):
 	velocity += direction
+	
+func setDisableCollision(disable_collision: bool):
+	_disable_collision = disable_collision
 
 func getDownVoxel():
 	return _getVoxel(Vector3.DOWN)
