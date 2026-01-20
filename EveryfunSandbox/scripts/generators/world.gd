4bd2221cf8edb7cc67f477cbd4c35251e8822796
igorkll <frerther@gmail.com>
2026-01-20 19:32:08 +0300

    Update world.gd

--- EveryfunSandbox/scripts/generators/world.gd
+++ EveryfunSandbox/scripts/generators/world.gd
@@ -34,6 +34,8 @@ var dirtHoise
 var resourcesNoises = []
 var terrainNoises = []
 
+var grassCutPos = 64 
+
 func createMountainNoises(seed):
 	var lnoise = FastNoise2.new()
 	lnoise.period = 5000
@@ -99,17 +101,19 @@ func _generate_block(buffer: VoxelBuffer, position: Vector3i, lod: int):
 					var terrainHeight = heightOffset + round(noiseValue * terrainHeightArr[0])
 					heightOffset = terrainHeight
 					
+					var grassCut = worldPos.y > grassCutPos
+					
 					var caveNoiseValue = (caveHoise.get_noise_3d_single(worldPos / caveScale) + 1) / 2
 					if caveNoiseValue < cavePercent:
 						buffer.set_voxel_v(0, localPos, VoxelBuffer.CHANNEL_TYPE)
-					elif worldPos.y == terrainHeight:
+					elif worldPos.y == terrainHeight && !grassCut:
 						buffer.set_voxel_v(id_grass, localPos, VoxelBuffer.CHANNEL_TYPE)
 					elif worldPos.y < terrainHeight:
 						var dirtNoiseValue = (dirtHoise.get_noise_2d_single(Vector2i(worldPos.x, worldPos.z)) + 1) / 2
 						dirtNoiseValue *= dirtHeight
 						dirtNoiseValue += dirtOffset
 						var dirtPos = terrainHeight - worldPos.y
-						if dirtPos <= dirtNoiseValue:
+						if dirtPos <= dirtNoiseValue  && !grassCut:
 							buffer.set_voxel_v(id_dirt, localPos, VoxelBuffer.CHANNEL_TYPE)
 						else:
 							var finded = false
